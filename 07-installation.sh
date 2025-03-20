fe4bbc903757aae1cca596d846d84d97bf82c160
hari998908 <harimaddineni2025@gmail.com>
2025-03-20 15:48:27 +0530

    Installation Commit

---
+++ 07-installation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [$USERID -ne 0]
+  then 
+     echo ("Run the script using Root Users")
+  else
+     ech ("Youbare the root User")   
+     exit 1
+fi
+
+dnf install mysql -y
+
+if [$? -ne 0]
+  then
+    echo ("My SQL installation...FAILED")
+  else
+    echo ("MY SQL installation....SUCCESSFUL")  
+fi
+
+dnf install git -y
+
+if [$? -ne 0]
+  then
+    echo ("Git installation ...FAILED")
+  else
+    echo ("Git Installation....SUCCESSFUL")
+ fi   
+    
