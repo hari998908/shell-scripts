a9f55fe7a3f70776a359617d604c0e2d3193915b
hari998908 <harimaddineni2025@gmail.com>
2025-03-20 14:22:09 +0530

    varible2 2nd Commit

--- 04-variables.sh
+++ 04-variables.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Enter User Name:"
-read -s $USERNAME
+read -s USERNAME
 
 echo "Enter Password:"
-read -s $PASSWORD
+read -s PASSWORD
 
 echo "User name is :$USERNAME,Password is $PASSWORD"
