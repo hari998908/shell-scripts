e58164352306475857fafed9c09a6722575de1f1
hari998908 <harimaddineni2025@gmail.com>
2025-03-20 15:25:02 +0530

    condition1 Commit

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -2,9 +2,11 @@
 
 number=$1
 
-if [$number -gte 10]
+if [$number -ge 10]
 then 
     echo "Given number $number is greater than or equal to 10"
 else
     echo "Given number $number is less than 10"
 fi
+
+##-gt, -lt, -eq, -ge, -le
