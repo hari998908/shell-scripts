b34708bb9ec59c393f0960501fe6931eeea4f807
hari998908 <harimaddineni2025@gmail.com>
2025-03-20 14:55:50 +0530

    array3 Commit

--- 05-arryas.sh
+++ 05-arryas.sh
@@ -5,5 +5,5 @@ names=("Hari" "Mamatha" "Chetahan" "Nivedha")
 echo "Print First Name : ${names[0]}" #prints first index
 echo "Print Second Name : ${names[1]}" #prints second index
 echo "Print Kids Names : ${names[*]:2:3}" #prints range of indexes
-echo "Print all Name ${names[@]}" #Prints all the indexes
+echo "Print all Names: ${names[@]}" #Prints all the indexes
 
