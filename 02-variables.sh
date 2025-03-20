000695025d7bed1d8f7855894a2b8dcaff0dc821
hari998908 <harimaddineni2025@gmail.com>
2025-03-20 13:32:32 +0530

    variables

---
+++ 02-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Delaration of Variables
+# Person1 = Hari
+# Person2 = Ramesh
+
+echo "Hari :: Hi Ramesh ! how are you?"
+echo "Ramesh :: Good ! How are you doing Hari?"
+echo "Hari :: Fine ! How is your job?"
+echo "Ramesh :: Not bad ! I woild like to switch my career into Devops"
