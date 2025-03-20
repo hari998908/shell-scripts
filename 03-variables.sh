5c6d6f594ee7da296c1e3fe78f066b44e84b2181
hari998908 <harimaddineni2025@gmail.com>
2025-03-20 14:13:52 +0530

    varible2 Commit

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Delaclaring variables
+
+Person1=$1
+Person2=$2
+
+echo "$Person1::Hi $Person2!How are you?"
+echo "$Person2::Good!How are you doing $Person1"
+echo "$Person1::Fine!How is your Job?"
+echo "$Person2::Not bad! I am thinking to swith my career in Devops"
