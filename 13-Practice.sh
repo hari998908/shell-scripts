4bd0a9bde12a0eb32432ef16453462f73e4ae85f
hari998908 <harimaddineni2025@gmail.com>
2025-07-26 12:15:00 +0530

    Practice17

--- 13-Practice.sh
+++ 13-Practice.sh
@@ -2,7 +2,7 @@
 USERID=$(id -u)
 TIMESTAMP=$(date +%F:%H:%M:%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=./$SCRIPT_NAME-$TIMESTAMP.log
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
 R="\e[31m"
 G="\e[32m"
@@ -29,13 +29,13 @@ else
     echo "Ypou are super user"
 fi
 
-dnf install mysql -y &>>LOGFILE
+dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MY SQL"
 
-dnf install git -y &>>LOGFILE
+dnf install git -y &>>$LOGFILE
 VALIDATE $? "Installing GIT"
 
-dnf install docker -y &>>LOGFILE
+dnf install docker -y &>>$LOGFILE
 VALIDATE $? "Installing Docker"
 
 
