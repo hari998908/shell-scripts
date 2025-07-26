f723d6757ae709a72ca149dc3e497dfe7f38caf6
hari998908 <harimaddineni2025@gmail.com>
2025-07-26 17:26:53 +0530

    loops

---
+++ expense-shell/14-loops.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+USERID=$(id -u)
+DATE=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[34m"
+N="\e[0m"
+
+echo "Scripts started Executing at : $DATE"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2...$G SUCCESS $N"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run script with Super User"
+    exit 1
+else
+    echo "You are the Super User"
+fi
+
+for i in $@
+do
+    echo "Package to Install: $i"
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -eq 0 ]
+    then
+        echo -e "$i already installed $Y...$N"
+        exit 1
+    else
+        dnf install $i &>>$LOGFILE
+        VALIDATE $? "Installation of $i"
+    fi
+done    
+
