f516eac229374616e9d456c9dd781358b9f07e40
hari998908 <harimaddineni2025@gmail.com>
2025-07-29 12:35:51 +0530

    expense7

--- expense-shell/backend.sh
+++ expense-shell/backend.sh
@@ -77,7 +77,7 @@ VALIDATE $? "Enabling backend"
 dnf install mysql -y &>>$LOGFILE
 VALIDATE $? "Installing MySQL Client"
 
-mysql -h  -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+mysql -h 172.31.21.254 -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
 VALIDATE $? "Schema loading"
 
 systemctl restart backend &>>$LOGFILE
