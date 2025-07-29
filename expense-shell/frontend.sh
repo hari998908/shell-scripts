380fd855ff7fc9df26d3c917ebc29dd1df709d93
hari998908 <harimaddineni2025@gmail.com>
2025-07-29 12:53:45 +0530

    expense9

--- expense-shell/frontend.sh
+++ expense-shell/frontend.sh
@@ -1,43 +1,40 @@
 #!/bin/bash
 
 USERID=$(id -u)
-DATE=$(date +%F:%H:%M:%S)
-SCRIPTNAME=$(echo $0 | cut -d "." -f1)
-LOGFILE=/tmp/$SCRIPTNAME-$DATE.log
-
-R="\e[31"
-G="\e[32"
-Y="\e[33"
-N="\e[0"
-
-echo "Script is started at: $DATE"
+TIMESTAMP=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 VALIDATE(){
-    if [ $1 ne 0 ]
-    then
-        echo -e "$2..$R FAILED $N"
+   if [ $1 -ne 0 ]
+   then
+        echo -e "$2...$R FAILURE $N"
         exit 1
     else
-        echo -e "$2..$G SUCCESS $N"
+        echo -e "$2...$G SUCCESS $N"
     fi
 }
 
 if [ $USERID -ne 0 ]
 then
-    echo "Please login with the Root User"
-    exit 1
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
 else
-    echo "You are the Root User"
+    echo "You are super user."
 fi
 
 dnf install nginx -y &>>$LOGFILE
-VALIDATE $? "Installing Nginx Server"
+VALIDATE $? "Installing nginx"
 
 systemctl enable nginx &>>$LOGFILE
-VALIDATE $? "Enabling Nginix"
+VALIDATE $? "Enabling nginx"
 
 systemctl start nginx &>>$LOGFILE
-VALIDATE $? "Starting Nginx"
+VALIDATE $? "Starting nginx"
 
 rm -rf /usr/share/nginx/html/* &>>$LOGFILE
 VALIDATE $? "Removing existing content"
@@ -49,6 +46,7 @@ cd /usr/share/nginx/html &>>$LOGFILE
 unzip /tmp/frontend.zip &>>$LOGFILE
 VALIDATE $? "Extracting frontend code"
 
+#check your repo and path
 cp /home/ec2-user/shell-scripts/expense-shell/expense.conf /etc/nginx/default.d/expense.conf &>>$LOGFILE
 VALIDATE $? "Copied expense conf"
 
